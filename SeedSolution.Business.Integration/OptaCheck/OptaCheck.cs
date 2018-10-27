2a185aa308a04bd6a43fcb71cd6eafa020b23ef9
Dennis Florencio Oxlaj Garcia <denisgarc@hotmail.com>
2018-10-26 19:42:12 -0600

    feat: carga inicial del proyecto

---
+++ SeedSolution.Business.Integration/OptaCheck/OptaCheck.cs
@@ -0,0 +1,14 @@
+﻿using SeedSolution.Business.Integration.Interfaces.OptaCheck;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SeedSolution.Business.Integration.OptaCheck
+{
+    public class OptaCheck : IOptaCheck
+    {
+    }
+}
---
+++ SeedSolution.IoC/Setup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StructureMap;
+using SeedSolution.Data.Connection;
+using SeedSolution.Data.Connection.Interfaces;
+using SeedSolution.Data.SecurityAcces;
+using SeedSolution.Data.Interfaces.SecurityAcces;
+using SeedSolution.Business.SecurityAccess;
+using SeedSolution.Business.Interfaces;
+
+namespace SeedSolution.IoC
+{
+    public class Setup
+    {
+        public static void ConfigureIoC(Action<ConfigurationExpression> MoreIoCConfigurations)
+        {
+            ObjectFactory.Configure(x =>
+            {   
+                x.For<IConnectionTools>().Use<MSSQLTools>();
+                x.For<ISysUser>().Use<SysUserDB>();
+                x.For<ISecurityAccesBL>().Use<SecurtiyAccesBL>();
+                //Más configuraciones
+                MoreIoCConfigurations(x);
+            });
+        }
+    }
+}
